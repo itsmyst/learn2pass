6e2cc8cb0a1e28371308bd8be8e2692ead9f24be
itsmyst <itsmyst>
2018-06-24 15:04:24 +0200

    Initial commit

---
+++ src/MainGUI.java
@@ -0,0 +1,16 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class MainGUI {
+    private JButton testButton;
+
+    public MainGUI() {
+        testButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("test!");
+            }
+        });
+    }
+}
